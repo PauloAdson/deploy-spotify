87c30c59ac05f7be07ffdb85976020c92803d614
Paulo Adson <paulo.alves200581@gmail.com>
2025-02-16 14:40:28 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,10 @@
 // Fetch ou Axios
 // Vamos usar o Axios, npm i axios no front
-
+import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
